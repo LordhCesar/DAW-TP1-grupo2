82607fec47c6fb37477746a51a9778aeef18b9b8
César Pedrolini <66392279+LordhCesar@users.noreply.github.com>
2022-06-05 13:55:27 -0300

    Update validacion.js

--- validacion.js
+++ validacion.js
@@ -6,44 +6,72 @@ function enviar() {
 
   let error = false;
   let mensajesError = "";
-  resetear();
 
-  //logica de validacion
+  const entradas = document.getElementById("entradas");
+  const apellido = document.getElementById("apellido");
+  const nombre = document.getElementById("nombre");
+  const email = document.getElementById("email");
+  const confirmationEmail = document.getElementById("confirmationEmail");
 
-  if (document.getElementById("nombre").value == "") {
+  if (entradas.value == "") {
     error = true;
-    mensajesError += "<p>El campo nombre es obligatorio</p>";
-    document.getElementById("nombre").className = "error"; //className agrega una clase
+    mensajesError += "<p>Debe ingresar una cantidad de entradas</p>";
+    entradas.className = "error";
+  }
+  if (apellido.value == "") {
+    error = true;
+    mensajesError += "<p>Debe ingresar el apellido</p>";
+    apellido.className = "error";
+  }
+  if (nombre.value == "") {
+    error = true;
+    mensajesError += "<p>Debe ingresar una cantidad de entradas</p>";
+    nombre.className = "error";
+  }
+  if (email.value == "") {
+    error = true;
+    mensajesError += "<p>Debe ingresar una cantidad de entradas</p>";
+    email.className = "error";
+  }
+  if (confirmationEmail.value == "") {
+    error = true;
+    mensajesError += "<p>Debe ingresar una cantidad de entradas</p>";
+    confirmationEmail.className = "error";
   }
 
-  if (document.getElementById("apellido").value.length == 0) {
-    error = true;
-    mensajesError += "<p>El campo apellido es obligatorio</p>";
-    document.getElementById("apellido").className = "error"; //className agrega una clase
-  }
 
-  if (document.getElementById("provincia").value == "0") {
-    error = true;
-    mensajesError += "<p>Selecciona una provincia</p>";
-  }
 
-  if (!document.getElementById("condiciones").checked) {
-    error = true;
-    mensajesError += "<p>Es obligatorio que aceptes los terminos</p>";
-  }
 
-  // getEelementById es el unico que devueve un unico elemento, todos los demas getElements devuelven un array
-  let opciones = document.getElementsByName("sexo");
-  let seleccionado = false;
-  for (i in opciones) {
-    if (opciones[i].checked) {
-      seleccionado = true;
-    }
-  }
-  if (!seleccionado) {
-    error = true;
-    mensajesError += "<p>Es obligatorio que indiques tu sexo</p>";
-  }
+//   if (document.getElementById("apellido").value.length == 0) {
+//     error = true;
+//     mensajesError += "<p>El campo apellido es obligatorio</p>";
+//     document.getElementById("apellido").className = "error"; //className agrega una clase
+//   }
+
+//   if (document.getElementById("provincia").value == "0") {
+//     error = true;
+//     mensajesError += "<p>Selecciona una provincia</p>";
+//   }
+
+//   if (!document.getElementById("condiciones").checked) {
+//     error = true;
+//     mensajesError += "<p>Es obligatorio que aceptes los terminos</p>";
+//   }
+
+//   // getEelementById es el unico que devueve un unico elemento, todos los demas getElements devuelven un array
+//   let opciones = document.getElementsByName("sexo");
+//   let seleccionado = false;
+//   for (i in opciones) {
+//     if (opciones[i].checked) {
+//       seleccionado = true;
+//     }
+//   }
+//   if (!seleccionado) {
+//     error = true;
+//     mensajesError += "<p>Es obligatorio que indiques tu sexo</p>";
+//   }
+
+
 
   //si hay errorer que se muestren
   //Si no los hay, que el formulario se envie
@@ -54,8 +82,5 @@ function enviar() {
     this.submit();
   }
 
-  function resetear() {
-    document.getElementById("nombre").className = "";
-    document.getElementById("apellido").className = "";
-  }
+ 
 }

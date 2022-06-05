b96abc6850c2db3478810f06b7be766adcd9f947
César Pedrolini <66392279+LordhCesar@users.noreply.github.com>
2022-06-05 16:09:24 -0300

    validaciones mejoradas

---
+++ validation.js
@@ -0,0 +1,145 @@
+let entradas = document.getElementById("entradas");
+entradas.addEventListener("blur", validarEntradas);
+let apellido = document.getElementById("apellido");
+apellido.addEventListener("blur", validarApellido);
+let nombre = document.getElementById("nombre");
+nombre.addEventListener("blur", validarNombre);
+let email = document.getElementById("email");
+email.addEventListener("blur", validarEmail);
+let confirmationEmail = document.getElementById("confirmationEmail");
+confirmationEmail.addEventListener("blur", validarConfirmationEmail);
+
+
+function validarEntradas() {
+    if (entradas.value == "") {
+        entradas.className = "error";
+        alert("Debe ingresar una cantidad de entradas");
+    }else{
+        entradas.className = "";
+    }
+  };
+
+function validarApellido() {
+    if (apellido.value == "") {
+        apellido.className = "error";
+        alert("Debe ingresar un apellido");
+    }else{
+        apellido.className = "";
+    }
+  };
+
+  function validarNombre() {
+    if (nombre.value == "") {
+        nombre.className = "error";
+        alert("Debe ingresar un nombre");
+    }else{  
+        nombre.className = "";
+    }
+  };
+
+  function validarEmail() {
+    if (email.value == "") {
+        email.className = "error";
+        alert("Debe ingresar un email");
+    }else{
+        email.className = "";
+    }
+  };
+
+  function validarConfirmationEmail() {
+    if (confirmationEmail.value == "") {
+        confirmationEmail.className = "error";
+        alert("Debe confirmar el email");
+    }else{
+        confirmationEmail.className = "";
+    }
+  };
+  
+
+
+
+
+const form = document.getElementById("form");
+form.addEventListener("submit", function (e){
+
+  e.preventDefault(); 
+
+  let error = false;
+  let mensajesError = "";
+
+  let entradas = document.getElementById("entradas");
+  let apellido = document.getElementById("apellido");
+  let nombre = document.getElementById("nombre");
+  let email = document.getElementById("email");
+  let confirmationEmail = document.getElementById("confirmationEmail");
+
+  if (entradas.value == "") {
+    error = true;
+    mensajesError += "Debe ingresar una cantidad de entradas\n";
+    entradas.className = "error";
+  }
+  if (apellido.value == "") {
+    error = true;
+    mensajesError += "Debe ingresar el apellido\n";
+    apellido.className = "error";
+  }
+  if (nombre.value == "") {
+    error = true;
+    mensajesError += "Debe ingresar el nombre\n";
+    nombre.className = "error";
+  }
+  if (email.value == "") {
+    error = true;
+    mensajesError += "Debe ingresar un email\n";
+    email.className = "error";
+  }
+  if (confirmationEmail.value == "") {
+    error = true;
+    mensajesError += "Debe confirmar el email ingresado\n";
+    confirmationEmail.className = "error";
+  }
+
+
+
+
+//   if (document.getElementById("apellido").value.length == 0) {
+//     error = true;
+//     mensajesError += "El campo apellido es obligatorio";
+//     document.getElementById("apellido").className = "error"; //className agrega una clase
+//   }
+
+//   if (document.getElementById("provincia").value == "0") {
+//     error = true;
+//     mensajesError += "Selecciona una provincia";
+//   }
+
+//   if (!document.getElementById("condiciones").checked) {
+//     error = true;
+//     mensajesError += "Es obligatorio que aceptes los terminos";
+//   }
+
+//   // getEelementById es el unico que devueve un unico elemento, todos los demas getElements devuelven un array
+//   let opciones = document.getElementsByName("sexo");
+//   let seleccionado = false;
+//   for (i in opciones) {
+//     if (opciones[i].checked) {
+//       seleccionado = true;
+//     }
+//   }
+//   if (!seleccionado) {
+//     error = true;
+//     mensajesError += "Es obligatorio que indiques tu sexo";
+//   }
+
+
+
+  //si hay errorer que se muestren
+  //Si no los hay, que el formulario se envie
+  if (error) {
+    alert(mensajesError);
+  } else {
+    this.submit();
+  }
+
+ 
+});
